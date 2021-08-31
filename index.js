b8f97b0e4bb6ce13ccea839580dbecf6f8c56a57
Patrick Wu <Ptrckwu@yahoo.com>
2021-08-31 12:44:39 -0400

    commit tree a138c719782e6f399009b25a64586f96beae91e8

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    return Math.abs(pickUpLocation - 42)
+};
+function distanceTravelledInFeet(pickUpLocation, destination) {
+    return Math.abs(264*(pickUpLocation - destination))
+};
+function distanceFromHqInFeet(pickUpLocation) {
+    return Math.abs(264* (pickUpLocation - 42))
+};
+function calculatesFarePrice(pickUpLocation, destination) {
+    const travelledFeet = distanceTravelledInFeet(pickUpLocation, destination)
+    if (travelledFeet <= 400) {
+        return 0
+    } else if (travelledFeet > 400 && travelledFeet <= 2000) {
+        return (travelledFeet - 400) * .02
+    } else if (travelledFeet <= 2500 && travelledFeet > 2000) {
+        return 25
+    } else if (travelledFeet > 2500) {
+        return `cannot travel that far`
+    }   
+};
